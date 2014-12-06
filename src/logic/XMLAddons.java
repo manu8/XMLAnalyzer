c3cbf8593107cddd2440ddfe1586d3448d975dea
Manuel Berrio <manuelbm8@gmail.com>
2014-12-06 14:23:21 +0100

    Adición de la clase XMLAddons

--- src/logic/XMLAddons.java
+++ src/logic/XMLAddons.java
@@ -4,24 +4,29 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
-import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
+import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
 public class XMLAddons {
-	public static NodeList executeXPathExpression(String expression, InputSource source) {
+	public static NodeList executeXPathExpression(String expression, Document document) {
 		NodeList nodes = null;
 		try {
-			nodes = (NodeList) XPathFactory.newInstance().newXPath().evaluate(expression, source, XPathConstants.NODESET);
+			nodes = (NodeList) XPathFactory.newInstance().newXPath().evaluate(expression, document, XPathConstants.NODESET);
 		} catch (XPathExpressionException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -39,15 +44,22 @@ public class XMLAddons {
 		}
 	}
 	
-	public static Transformer transformXMLWithXSLT(String xlstpath, InputSource document) {
+	public static void transformXMLWithXSLT(String xlstpath, String outpath, Document document) {
 		File stylesheet = null;
 		if(xlstpath != ""){
 			stylesheet = new File(xlstpath);
 		}
-		Transformer transformer = null;
 		StreamSource stylesource = new StreamSource(stylesheet);
+		Source source = new DOMSource(document);
+		Result out = new StreamResult(new File(outpath));
 		try {
-			transformer = TransformerFactory.newInstance().newTransformer(stylesource);
+			Transformer transformer = TransformerFactory.newInstance().newTransformer(stylesource);
+			try {
+				transformer.transform(source, out);
+			} catch (TransformerException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		} catch (TransformerConfigurationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -55,6 +67,5 @@ public class XMLAddons {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return transformer;
 	}
 }
