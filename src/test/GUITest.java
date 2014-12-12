64979cfcc66b8d696b90c4e69fd03e95fb1d0028
Andres Moreno <andresmore84@gmail.com>
2014-12-12 19:07:01 +0100

    GUITest

--- src/test/GUITest.java
+++ src/test/GUITest.java
@@ -1,11 +1,25 @@
 package test;
 
+import java.awt.EventQueue;
+
 import ui.GUI;
 
 public class GUITest {
 
 	public static void main(String[] args) {
-		new GUI();
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI frame = new GUI();
+					frame.setVisible(true);
+					frame.setSize(950, 700);
+					frame.setResizable(false);
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 }
